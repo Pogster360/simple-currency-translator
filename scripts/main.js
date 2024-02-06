a9cb5fb401d3be05a76ddc8a66559958ddfbdacf
Pogster360 <petergregorczyk@gmail.com>
2024-02-06 14:13:26 +1100

    variables added to js
    created variables for js, i had to change input style to P to display results

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,27 @@
-// boilerplate for main js currently
+// create variables for each currency type
+
+// grabbing the result input fields to display results later
+const copperResult = document.getElementById("copperResult");
+const silverResult = document.getElementById("silverResult");
+const goldResult = document.getElementById("goldResult");
+const platinumResult = document.getElementById("platinumResult");
+
+// getting the input to calculate
+const copperInput = document.getElementById("copperInput");
+const silverInput = document.getElementById("silverInput");
+const goldInput = document.getElementById("goldInput");
+const platinumInput = document.getElementById("platinumInput");
+
+// button variable for actionlistner
+const calculate = document.getElementById("calculate");
+
+function results() {
+    copperResult.textContent = copperInput.value;
+    silverResult.textContent = silverInput.value;
+    goldResult.textContent = goldInput.value;
+    platinumResult.textContent = platinumInput.value;
+}
+
+// simple function to test inputs elements
+calculate.addEventListener("click", results);
+
